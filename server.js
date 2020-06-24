0e0be10a88d3936ef73b7b1938191c2e7e27c6ec
Eliott R Davidson <fussypuppet@Eliotts-MacBook-Pro.local>
2020-06-24 15:33:08 -0700

    finished part1, beginning part 2

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const axios = require('axios');
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
@@ -15,11 +16,46 @@ app.use(ejsLayouts);
 // Adds some logging to each request
 app.use(require('morgan')('dev'));
 
-// Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
-});
+function errorHandler(error) {
+  console.log("🟥🟥🟥🟥 Error! 🟥🟥🟥🟥");
+  console.log(error);
+}
 
+// Routes
+app.get('/', (req,res) => {
+  res.render("");
+})
+
+app.get('/results', (req,res) => {
+  //console.log( `Sending API requesat to http://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=${req.query.q}`);
+  axios.get(`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${req.query.q}`)
+  .then(function(response){
+    //console.log(response.data);
+    let queryResults = response.data.Search;
+    res.render('results', {queryResults});
+  }).catch(errorHandler);
+})
+
+app.get('/movies/:id', (req,res) => {
+  axios.get(`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&i=${req.params.id}`)
+  .then(function(response){
+    let thisResult = response.data;
+    res.render('detail', {thisResult})
+  }).catch(errorHandler);
+})
+
+
+/*app.get('/', function(req,res){
+  axios.get(`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=Star+Wars`)
+      .then(function(response){
+          //let queryResponse = stringify(response);
+          let result = response.data;
+          res.send(result);
+      }).catch(function(err){
+          console.log(err);
+      })
+})
+*/
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
 
